532fefce960e1e38e06cac81f2468a8fbccac5eb
RAVICHANDRAKALIKI <k.ravichandrareddy@gmail.com>
2022-09-15 13:42:31 -0400

    link with cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "bun8kb",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
